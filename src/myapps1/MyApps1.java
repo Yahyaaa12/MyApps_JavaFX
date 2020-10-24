d465c7c8e1d87c7a0c3e3ccb952d767674ebe4e3
Yahyaaa12 <50857033+Yahyaaa12@users.noreply.github.com>
2020-10-24 08:21:26 +0700

    tambah tampilan GUI Form_Pendaftaran_Event

--- src/myapps1/MyApps1.java
+++ src/myapps1/MyApps1.java
@@ -6,46 +6,92 @@
 package myapps1;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.TilePane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 /**
  *
- * @author Mantaf
+ * @author Mantaf (Moch Mahfudz Yahya | Eksc1 | 19201226)
  */
 public class MyApps1 extends Application {
     
-    @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
+    public void start(Stage window){
+        //// --- VBox ---
+        //// VBox index = new VBox();
+        //// Scene scene_dasar = new Scene(index, 400, 400);
+                
+        //// --- GridPane ---
+        GridPane index = new GridPane();
+        index.setMinSize(400,200);
+        index.setPadding(new Insets(10,10,10,10));
+        index.setVgap(10);
+        index.setHgap(10);
+        index.setAlignment(Pos.CENTER);
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        //// Form Nama 
+        Text t_nama = new Text("Nama");
+        Text titik_1 = new Text(":");
+        TextField tf_nama = new TextField();
         
-        Scene scene = new Scene(root, 300, 250);
+        // Form Telepon 
+        Text t_telepon = new Text("Telepon");
+        Text titik_2 = new Text(":");
+        TextField tf_telepon = new TextField();
         
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        launch(args);
+        // Form Alamat 
+        Text t_alamat = new Text("Alamat");
+        Text titik_3 = new Text(":");
+        TextField tf_alamat = new TextField();
+        
+        // Tombol 
+        TilePane tpane = new TilePane();
+        Button btKirim = new Button("Kirim");
+        Button btHapus = new Button("Hapus");
+        tpane.setOrientation(Orientation.HORIZONTAL);
+        tpane.setAlignment(Pos.CENTER);
+        tpane.getChildren().addAll(btKirim, btHapus);
+        
+        // --- Initial To Grip Pane ---
+       
+        // Nama
+        index.add(t_nama,0,0);
+        index.add(titik_1,1,0);
+        index.add(tf_nama,2,0);
+        
+        // Telepon
+        index.add(t_telepon,0,1);
+        index.add(titik_2,1,1);
+        index.add(tf_telepon,2,1);
+        
+        // Alamat
+        index.add(t_alamat,0,2);
+        index.add(titik_3,1,2);
+        index.add(tf_alamat,2,2);
+        
+        // Tombol
+        index.add(tpane,2,3);
+        
+        // Window Title
+        window.setTitle("Form Pendaftaran Event");
+        
+        // Call Window
+        Scene scene_dasar = new Scene(index);
+        window.setScene(scene_dasar);
+        
+        // Show Windows
+        window.show();
     }
     
+    public static void main(String[] args){
+        Application.launch(args);
+    }
 }
